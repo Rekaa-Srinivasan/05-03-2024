b6fb4baffb3b6d248f71af4a6a5f2b19eea33add
Rekaa-Srinivasan <rekaa.srinivasan@numentica-ui.com>
2024-03-05 16:55:32 +0530

    Updated removeDuplicates.js

--- ex08_RemoveDuplicates.js
+++ ex08_RemoveDuplicates.js
@@ -1,4 +1,4 @@
 function removeDuplicates(array){
     return new Set(array);
 }
-console.log(removeDuplicates(1, 'two', 'two', 7, '7', 11));
+console.log(removeDuplicates([1, 'two', 'two', 7, '7', 11]));
