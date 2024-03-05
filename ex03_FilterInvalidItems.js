9aae44c4ae34bb214c3e088316f2e43c896532c3
Rekaa-Srinivasan <162275345+Rekaa-Srinivasan@users.noreply.github.com>
2024-03-05 18:12:44 +0530

    Updated problem 3

--- ex03_FilterInvalidItems.js
+++ ex03_FilterInvalidItems.js
@@ -1,10 +1,13 @@
-function filterOut(arr, s){
-    const a = [];
-    for(let i of arr){
-        if(typeof i!=s){
-            a.push(i);
+function filterOut(array, datatype){
+    const result = [];
+    for(let element of array){
+        if(typeof element!=datatype){
+            result.push(element);
         }
     }
-    console.log(a);
+    console.log(result);
 }
-filterOut(['a', 'd', 1, true, 'cd', 33], 'string');
+filterOut(['a', 'd', 1, true, 'cd', 33], 'string'); // [ 1, true, 33 ]
+filterOut(['a', 'd', 1, true, 'cd', null], 'string'); // [ 1, true, null ]
+filterOut(['a', '', 1, true, 'cd', 33], 'string'); // [ 1, true, 33 ]
+filterOut(['a', 'd', 1, undefined, 'cd', 0], 'string'); // [ 1, undefined, 0 ]
