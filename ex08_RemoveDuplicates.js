60b66958f644580588522f07381e2c0af4fb35ac
Rekaa-Srinivasan <rekaa.srinivasan@numentica-ui.com>
2024-03-05 17:16:01 +0530

    Updated ex08...js

--- ex08_RemoveDuplicates.js
+++ ex08_RemoveDuplicates.js
@@ -1,4 +1,15 @@
 function removeDuplicates(array){
-    return new Set(array);
+    // return new Set(set); // Passing an array in a Set() constructor will create anew set with unique values of an array (OR)
+    const uniqueElements = new Set();
+    for(let counter in array){
+        if(!uniqueElements.has(array[counter])){
+            uniqueElements.add(array[counter]);
+        }
+    }
+    return uniqueElements;
 }
-console.log(removeDuplicates([1, 'two', 'two', 7, '7', 11]));
+console.log(removeDuplicates([1, 'two', 'two', 7, '7', 11])); // { 1, 'two', 7, '7', 11 }
+console.log(removeDuplicates([1, 'two', 'two', 7, '7', 11, null])); // { 1, 'two', 7, '7', 11, null }
+console.log(removeDuplicates(null)); // {}
+console.log(removeDuplicates([])); // {}
+console.log(removeDuplicates([1, 'two', 'two', -0, '', 11])); // { 1, 'two', 0, '', 11 }
